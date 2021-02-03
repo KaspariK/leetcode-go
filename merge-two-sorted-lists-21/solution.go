ba2ee3c11ee8444518ae07cd47a632bfbb1bedc5
Kevin Kaspari <KKASPARI@goremutual.ca>
2021-02-02 20:54:00 -0500

    Add solution and tests for merge-two-sorted-lists-21

--- merge-two-sorted-lists-21/solution.go
+++ merge-two-sorted-lists-21/solution.go
@@ -5,12 +5,47 @@ type ListNode struct {
     Next *ListNode
 }
 
+// iterative
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    temp := ListNode{}
+    head := &ListNode{}
+    prev := head
 
-    if l1.Val < l2.Val {
-        temp.Val = l1.Val
+    for l1 != nil || l2 != nil {
+        if l1 == nil {
+            prev.Next = l2
+            break
+        }
+        if l2 == nil {
+            prev.Next = l1
+            break
+        }
+
+        if l1.Val < l2.Val {
+            prev.Next = l1
+            l1 = l1.Next
+        } else {
+            prev.Next = l2
+            l2 = l2.Next
+        }
+
+        prev = prev.Next
     }
 
-    return &temp
+    return head.Next
 }
+
+// recursive
+//func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+//    if l1 == nil {
+//        return l2
+//    }
+//    if l2 == nil {
+//        return l1
+//    }
+//    if l1.Val < l2.Val {
+//        l1.Next = mergeTwoLists(l1.Next, l2)
+//        return l1
+//    }
+//    l2.Next = mergeTwoLists(l1, l2.Next)
+//    return l2
+//}
